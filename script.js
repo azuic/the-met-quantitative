df72332890154faf0380c9ccd04331f9abecc018
azuic <root@DESKTOP-626LSG0.localdomain>
2019-10-17 08:02:58 -0400

    removed map form

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const winH = window.innerHeight;
 // const h = 900;
 
 const w = winW;
-const h = winH;
+const h = 1000;
 
 const doMapping = (m, a, b, x, y) => {
     return (m*y-a*y+b*x-m*x)/(b-a);
 };
 
-const app = new PIXI.Application({ transparent: true, width: w, height: h, antialias:true});
+const app = new PIXI.Application({ transparent: true, width: w, height: h, antialias:true, resolution:2});
 document.body.appendChild(app.view);
 // PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.LINEAR;
 // PIXI.settings.RENDER_OPTIONS={antialias:true, resolution:2};
@@ -20,21 +20,34 @@ app.stage.addChild(container);
 const colorBlocks = new PIXI.Graphics();
 
 const map = [];
+let yTotal=100;
+
+let title = new PIXI.Text('Index of Colors',{fontFamily : 'Work Sans', fontWeight: 600, fontSize:90, fill : 0x384e68, align : 'center'});
+title.x=150;
+title.y=40;
+app.stage.addChild(title);
 for (let i=0; i<countryData.length;i++){
     // each country shape
     // const country = PIXI.Sprite.from(`assets/${countryData[i].isoCode}.png`);
-    const country = new PIXI.Text(countryData[i].country, {
+    let country = new PIXI.Text(countryData[i].country, {
                                      fontFamily: 'Work Sans',
-                                     fontSize: doMapping(countryData[i].size,2180,3600950,8,112),
+                                     fontSize: doMapping(countryData[i].size,2180,3600950,8,100),
                                      fontWeight: 500,
                                      fill: 0x384e68,
                                      align:'center'});
-    country.anchor.set(0.5);
+    country.anchor.set(0);
     // country.scale.set(doMapping(countryData[i].size,2180,3600950,0.02,0.5));
-    country.x = doMapping(countryData[i].latLon[1],-102.553,174.886,150,w-150);
-    country.y = doMapping(countryData[i].latLon[0],-56.264,40.901,h-50, 200);
+    // country.x = doMapping(countryData[i].latLon[1],-102.553,174.886,150,w-150);
+    // country.y = doMapping(countryData[i].latLon[0],-56.264,40.901,h-50, 200);
+    country.x=100;
+    if (i===0){
+        country.y=yTotal
+    }else{
+        country.y=yTotal+doMapping(countryData[i-1].size,2180,3600950,8,112);
+    }
+    yTotal=country.y;
     // country.tint = 0x384e68;
-    country.alpha = doMapping(countryData[i].objectCount, 1, 2616,0.2,1);
+    country.alpha = doMapping(countryData[i].objectCount, 1, 2616,0.4,0.9);
     country.speed = Math.random()*0.005;
     country.offset = Math.random()*10;
     country.interactive = true;
@@ -59,18 +72,18 @@ function onTouch(){
     // const side = doMapping(objectList.length,1,2626,50,5);
     const side = 20;
     // console.log(side);
-    const startX=150;
-    const startY=100;
+    const startX=450;
+    const startY=250;
     let cX=startX;
     let cY=startY;
-    const radius=8;
+    const radius=12;
     const lineWidth=5;
     // const gapX=(w-400)/550;
     // const gapY=(h-400)/500;
-    const gap=2*radius+8;
-    const nCol=Math.floor((w-300)/gap);
-    const nRow=Math.floor((h-200)/gap);
-
+    const gap=2*radius+12;
+    // const nCol=Math.floor((w-500)/gap);
+    // const nRow=Math.floor((h-200)/gap);
+    // console.log(nCol,nRow);
 
     for (let i=0; i<objectList.length; i++){
         // const blockX = Math.random()*1000+(winW-1000)/2;
@@ -87,9 +100,9 @@ function onTouch(){
         // colorBlocks.beginFill(parseInt(objectData[objectList[i]].colors[3],16));
         // colorBlocks.drawRect(blockX+3*side*objectData[objectList[i]].percents[0]+3*side*objectData[objectList[i]].percents[1]+3*side*objectData[objectList[i]].percents[2], blockY,3*side*objectData[objectList[i]].percents[3],side);
         // colorBlocks.endFill();
-        cX=startX+(i%nCol-1)*gap;
-        cY=startY+Math.floor(i/nRow-1)*gap;
-        console.log(Math.floor(i/500));
+        cX=startX+(i%40)*gap;
+        cY=startY+(Math.floor(i/60))*gap;
+        // console.log(i%nCol,Math.floor(i/nRow));
         colorBlocks.lineStyle(lineWidth,parseInt(objectData[objectList[i]].colors[0],16),1);
         colorBlocks.arc(cX,cY,radius,0,PIXI.PI_2*objectData[objectList[i]].percents[0]);
         colorBlocks.lineStyle(lineWidth,parseInt(objectData[objectList[i]].colors[1],16),1);
